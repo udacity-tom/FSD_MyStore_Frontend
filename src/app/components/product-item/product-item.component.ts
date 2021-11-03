b7fc63b0f9977d210eef9a31c0a3f178329ae1bd
udacity-tom <udacity@iamtom.com>
2021-11-03 10:44:07 +0100

    Refactor: moved item details rendering to product-item component

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/Product';
 
 @Component({
   selector: 'app-product-item',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent implements OnInit {
+  @Input() product: Product;
 
-  constructor() { }
+  constructor() { 
+    this.product = {
+      id: 0,
+      name: '',
+      url: '',
+      price: 0,
+      description: '',
+      category: ''
+    }
+  }
 
   ngOnInit(): void {
   }
