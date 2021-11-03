3d5a4b084400c1187879e4af6b79e89bf78ef6e7
udacity-tom <udacity@iamtom.com>
2021-11-03 23:41:45 +0100

    Feat/Style: added/styled Home component, page header title

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  //setup login name based on login status
+  loginStatus = 'Login';
+  blurb = 'Our range of products will entice your desires and line our pockets.'
+  moreBlurb = 'When you\'ve filled your Cart with our wares, please check out for a rapid delivery';
 
   constructor() { }
 
