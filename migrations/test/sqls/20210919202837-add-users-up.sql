4d5418a2d3a28e1ca18832bae606485354913c4d
udacity-tom <udacity@iamtom.com>
2021-11-08 23:25:14 +0100

    Feat: extend user data for DB/address/etc

--- migrations/test/sqls/20210919202837-add-users-up.sql
+++ migrations/test/sqls/20210919202837-add-users-up.sql
@@ -1,6 +1,6 @@
-INSERT INTO users(id, username, firstname, lastname, password) VALUES(1, 'Barney', 'Benny', 'Scholpferb', '%%$$$$$9adkaj');
-INSERT INTO users(id, username, firstname, lastname, password) VALUES(2, 'Hella', 'Helen', 'Batrib', '$2b$10$vdxOw56kdXTjuh.v8IvlPOF1fu3yvleynLYYbkJw8qPEBeIVWCuSG');
-INSERT INTO users(id, username, firstname, lastname, password) VALUES(3, 'Jakey', 'Jake', 'Grossenpfiff', '$2b$10$Nwz5HSm8IGxvPBKrtWFmHOEfMqoIlwI6dBB7DMyFalZISMTSqf.MW');
-INSERT INTO users(id, username, firstname, lastname, password) VALUES(4, 'Will', 'William', 'Burk', '$2b$10$6Scfn2HTUNtQKStr6G1meeOcWkbCoy2kcQdflcbR9rJ2105GfQogG');
-INSERT INTO users(id, username, firstname, lastname, password) VALUES(5, 'Bill', 'William', 'Burk', '$2b$10$K/JK3x0..R.PeLZSuZMrHuRzgIP2D8RPxzOXSTMoDqxaigC6XxFum');
+INSERT INTO users(id, admin, username, firstname, lastname, password, houseNum, street1, street2, city, postcode, country) VALUES(1, true, 'Barney', 'Benny', 'Scholpferb', '%%$$$$$9adkaj', '15', 'Barnicle Drive', 'West Wheatton', 'Arkansas', '22938', 'USA');
+INSERT INTO users(id, admin, username, firstname, lastname, password, houseNum, street1, street2, city, postcode, country) VALUES(2, false, 'Hella', 'Helen', 'Batrib', '$2b$10$vdxOw56kdXTjuh.v8IvlPOF1fu3yvleynLYYbkJw8qPEBeIVWCuSG', '338993', 'Apartment 5C', 'Holegrab Road', 'Nuneasis', 'PI M0N', 'Ireland');
+INSERT INTO users(id, admin, username, firstname, lastname, password, houseNum, street1, street2, city, postcode, country) VALUES(3, false, 'Jakey', 'Jake', 'Grossenpfiff', '$2b$10$Nwz5HSm8IGxvPBKrtWFmHOEfMqoIlwI6dBB7DMyFalZISMTSqf.MW', 'Falling Cottage', 'Arbanzis Street', '', 'Headonors', 'Rakemund', 'USA');
+INSERT INTO users(id, admin, username, firstname, lastname, password, houseNum, street1, street2, city, postcode, country) VALUES(4, false,  'Will', 'William', 'Burk', '$2b$10$6Scfn2HTUNtQKStr6G1meeOcWkbCoy2kcQdflcbR9rJ2105GfQogG', 'Flat 4', 'Block 7', 'Reynolds Street', 'Watterborne', '998223', 'United Kingdom');
+INSERT INTO users(id, admin, username, firstname, lastname, password, houseNum, street1, street2, city, postcode, country) VALUES(5, false,  'Bill', 'William', 'Burk', '$2b$10$K/JK3x0..R.PeLZSuZMrHuRzgIP2D8RPxzOXSTMoDqxaigC6XxFum', '230', 'Ambledon Road', '', 'Haverwille', 'PP0 RJS', 'Gibralter');
 ALTER SEQUENCE users_id_seq RESTART WITH 6;
