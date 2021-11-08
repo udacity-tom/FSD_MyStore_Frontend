2c7565ac63bc0ccfa446c84915255298899929da
udacity-tom <udacity@iamtom.com>
2021-11-08 14:41:49 +0100

    Feat: extended dataset for test data

--- migrations/sqls/20210917165455-users-table-up.sql
+++ migrations/sqls/20210917165455-users-table-up.sql
@@ -3,5 +3,11 @@ id serial PRIMARY KEY,
 username VARCHAR(64) NOT NULL, 
 firstname VARCHAR(64) NOT NULL,
 lastname VARCHAR(64) NOT NULL,
-password TEXT
+password TEXT,
+housenum NUMERIC(20),
+street1 TEXT,
+street2 TEXT,
+city TEXT,
+postcode TEXT,
+country TEXT
 );
