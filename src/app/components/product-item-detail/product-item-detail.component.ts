e0f4bc323f34fd795dd590ff0bb244e62fa1d75f
udacity-tom <udacity@iamtom.com>
2021-11-08 14:33:55 +0100

    Feat: added getDescription() for wiki text

--- src/app/components/product-item-detail/product-item-detail.component.ts
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -3,25 +3,17 @@ import { Product } from 'src/app/models/Product';
 import { ProductService } from 'src/app/service/products.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Observable } from 'rxjs';
-import { first } from 'rxjs/operators'
-// import { next } from 'rxjs';
-// import { map } from 'rxjs/operators';
-
 @Component({
   selector: 'app-product-item-detail',
   templateUrl: './product-item-detail.component.html',
   styleUrls: ['./product-item-detail.component.css']
 })
 export class ProductItemDetailComponent implements OnInit {
-  // @Input() products: Product[] = [];
   @Input() products: Product[] = [];
-  // @Input() productToView: Product = {};
-  // @Input() product: Product;
-  // productDetail:
   product: Product;
   givenId: number;
-  noOfItems: number = 1;
-  filter$!: Observable<string>;
+  noOfItems: number;
+  itemDescription: object;
 
   constructor(private productService:ProductService, private route: ActivatedRoute) { 
     this.product = {
@@ -34,39 +26,26 @@ export class ProductItemDetailComponent implements OnInit {
       accreditation: '',
       category: ''
     };
-
-    // this.productToView = {
-    //   id: 0,
-    //   name: '',
-    //   url: '',
-    //   price: 0,
-    //   snippet: '',
-    //   description: '',
-    //   accreditation: '',
-    //   category: ''
-    // };
-    
     this.givenId = 0;
+    this.noOfItems = 1;
+    this.itemDescription = {};
   }
   
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.givenId = Number(this.route.snapshot.paramMap.get('id'));
-    this.productService.getProducts().pipe(first()).subscribe(res => {
-      this.products = res;
-      // console.log("this.products from product-item-detail.component", this.products);
-    })
-    
-    setTimeout(() =>
-    {
-      this.product = this.products.filter( item => {
-      if(item.id != Number(this.givenId)){
-        return;
-      } else {
-        return item;
-      }
-    })[0];
-    
-  }, 250);
+    this.productService.getProduct(this.givenId).subscribe(res => {
+      this.product = res;
+    //   this.productService.getDescription(this.product.description, this.product.name).subscribe(res => {
+    //     this.itemDescription = res;
+    //   console.log("this.itemDescription product-item-detail", this.itemDescription);
+    // })
+    });
+
+
+    // this.productService.getDescription(this.product.description, this.product.name).subscribe(res => {
+    //   console.log("res from wiki", res);
+    //   this.itemDescription = res.query.pages.extracts;
+    // })
   }
 
   increaseItems(): void {
