c4ff948d6009ba7f55a3b0592b89c3f946c3f24c
udacity-tom <udacity@iamtom.com>
2021-11-15 23:12:49 +0100

    Feat: add register new user method

--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,7 @@ export class LoginService {
   url: string = '';
   returnedJWT: string = '';
   tokenPromise: string = '';
-  // req: string = {}
+  
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
@@ -24,15 +25,40 @@ export class LoginService {
     })
   }
 
-  constructor(private http: HttpClient) { 
+  constructor(private http: HttpClient, private tokenService: TokenService) { 
     this.username = '';
     this.password = '';
     this.currentJWT = '';
   }
 
   authUser(username: string, password: string): Observable<ArrayBuffer>{
-    this.url = `${this.protocol}${this.apiServer}:${this.apiPort}/users/authenticate`
+    this.url = `${this.protocol}${this.apiServer}:${this.apiPort}/users/authenticate`;
       return this.http.post< any >(this.url, {username: username, password: password}, {observe: 'body', responseType: 'json'});
     }
+
+  isLoggedIn() {
+    //observable to check if use is logged in
+    //checks, if valid token is present in local storage
+    //checks if token is still valid
+  }
+  
+  logOut() {
+    //logs user out, essentially deleting localstorage token
+  }
+  //simple user registration
+  registerNewUser(username: string, firstname: string, lastname: string, password: string): Observable<object> {
+    this.url = `${this.protocol}${this.apiServer}:${this.apiPort}/users/create`;
+    console.log('login service user register', {username: username, firstname: firstname, lastname:lastname, password: password});
+    return this.http.post<object> (this.url, {username: username, firstname: firstname, lastname:lastname, password: password}, {observe: 'body', responseType: 'json'});
+  }
+    // isLoggedIn():Observable<string> {
+    
+  //   return 
+  // }
+
+  
+
+
+
   }
 
