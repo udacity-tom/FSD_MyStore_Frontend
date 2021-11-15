9325762afd53f7965a2ae63812d7695920ddb1a7
udacity-tom <udacity@iamtom.com>
2021-11-15 23:11:56 +0100

    Feat: register new user component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/service/login.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  username: string = '';
+  firstname: string = '';
+  lastname: string = '';
+  password: string = '';
+  returnedJWT: object = {};
+  token: string = '';
+  authFn: object = {};
+  loading: boolean = false;
 
-  constructor() { }
+  constructor(private loginAuth: LoginService, private tokenService: TokenService) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    this.loading = true;
+    this.returnedJWT = this.registerUser(this.username, this.firstname, this.lastname, this.password);
+    this.loading = false;
+  }
+
+  registerUser(username: string, firstname: string, lastname: string, password: string ): object {
+    console.log('register component', username, firstname, lastname, password);
+    return this.loginAuth.registerNewUser( username, firstname, lastname, password ).subscribe();
   }
 
 }
