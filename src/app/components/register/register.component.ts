92dae243d636590751cd2f92f19bb326755e5fc5
udacity-tom <udacity@iamtom.com>
2021-11-16 18:03:18 +0100

    Fix/Feat: fix user method functionality/store JWT/exp

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -16,6 +16,13 @@ export class RegisterComponent implements OnInit {
   token: string = '';
   authFn: object = {};
   loading: boolean = false;
+  returnedRes: object = {};
+  newUser: object = {};
+  newtoken: string = '';
+  payload: object = {};
+  tokenToSet: object = {};
+  exp: number = 0;
+
 
   constructor(private loginAuth: LoginService, private tokenService: TokenService) { }
 
@@ -24,13 +31,22 @@ export class RegisterComponent implements OnInit {
 
   onSubmit(): void {
     this.loading = true;
-    this.returnedJWT = this.registerUser(this.username, this.firstname, this.lastname, this.password);
+    this.returnedRes = this.registerUser(this.username, this.firstname, this.lastname, this.password);
+     // this.returnedJWT = this.returnedRes[1] as object;
+    // console.log('returned res from api on user create', this.returnedRes);
     this.loading = false;
   }
-
+  
   registerUser(username: string, firstname: string, lastname: string, password: string ): object {
-    console.log('register component', username, firstname, lastname, password);
-    return this.loginAuth.registerNewUser( username, firstname, lastname, password ).subscribe();
+    // console.log('register component', username, firstname, lastname, password);
+    return this.loginAuth.registerNewUser( username, firstname, lastname, password ).subscribe(
+      res => {   //TO-DO: this MUST be refactored, something to with the interface?
+        this.returnedRes = res;
+        this.newtoken = Object.values(this.returnedRes)[1];
+        this.payload = Object.values(this.returnedRes)[2].payload;
+        this.exp = Object.values(this.payload)[13];
+        this.tokenService.setToken({token: this.newtoken, expiry: this.exp});
+      }
+    );
   }
-
 }
