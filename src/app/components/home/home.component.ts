4fc85812e6867bb33ae57416ff343f69639cfee5
udacity-tom <udacity@iamtom.com>
2021-11-18 18:16:23 +0100

    Feat: Logout menu after JWT issued

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   //setup login name based on login status
   loginStatus = 'Login';
-  blurb = 'Our range of products will entice your desires and line our pockets.'
-  moreBlurb = 'When you\'ve filled your Cart with our wares, please check out for a rapid delivery';
+  blurb = 'Our range of products will entice your desires.'
+  moreBlurb = 'When you\'ve filled your Cart with our wares, please check out for a rapid delivery. \nRegister now to speed the check-out process.';
 
   constructor() { }
 
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/service/login.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  loginStatus: boolean;
 
-  constructor() { }
+
+  constructor(private loginAuth: LoginService ) { 
+    this.loginStatus = false;
+  }
 
   ngOnInit(): void {
+    this.updateLoginStatus();
+  }
+
+  updateLoginStatus(): void {
+    this.loginAuth.loginStatus().subscribe(res => {
+      this.loginStatus = res;
+    })
+    console.log('Navbar loginSTatus', this.loginStatus);
   }
 
 }
