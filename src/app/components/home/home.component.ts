abab66cda4ab7b6df7bed63f558ce4ced9beeeaa
udacity-tom <udacity@iamtom.com>
2021-11-22 21:02:12 +0100

    Feat: update login status/token on home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { TokenService } from 'src/app/service/token.service';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/service/login.service';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   //setup login name based on login status
-  loginStatus = 'Login';
-  blurb = 'Our range of products will entice your desires.'
-  moreBlurb = 'When you\'ve filled your Cart with our wares, please check out for a rapid delivery. \nRegister now to speed the check-out process.';
+  loginStatus: boolean = false;
+  blurb:string = 'Our range of products will entice your desires.'
+  moreBlurb:string = 'When you\'ve filled your Cart with our wares, please check out for a rapid delivery. \nRegister now to speed the check-out process.';
+  username: string = '';
 
-  constructor() { }
+
+  constructor(private loginAuth: LoginService, private router: Router, private tokenService: TokenService) { }
 
   ngOnInit(): void {
+    this.updateLoginStatus();
+    this.tokenService.getToken();
+  }
+  
+  onChanges(): void {
+    this.updateLoginStatus();
+    this.tokenService.getToken();
+  }
+
+  updateLoginStatus(): void {
+    this.loginAuth.loginStatus().subscribe(res => {
+      this.loginStatus = res;
+      if(!this.loginStatus){
+        console.log('logged-in component re-route page, this.loginStatus is ', this.loginStatus);
+        // this.router.navigate(['/', {relativeTo: this.route}]);
+        this.router.navigate(['/']);
+        return;
+      }
+      this.tokenService.getToken().subscribe(res => {
+              this.username = res.user;
+            })
+    })
+    console.log('logged-in componet loginStatus', this.loginStatus);
   }
 
 }
