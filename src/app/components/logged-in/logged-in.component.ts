c91aafb1ff81756621d52d3f7f87781e3732d484
udacity-tom <udacity@iamtom.com>
2021-11-24 10:23:24 +0100

    Fix: filter fixed for active/complete, cleanup

--- src/app/components/logged-in/logged-in.component.ts
+++ src/app/components/logged-in/logged-in.component.ts
@@ -22,10 +22,6 @@ export class LoggedInComponent implements OnInit {
   authFn: object = {};
   loading: boolean = false;
   currentOrders: Order[] = [];
-  
-  // noOfCompletedOrders: Order[] = this.currentOrders.filter(el => {
-  //   el.status = 'complete'
-  // });
   noOfCompletedOrders: Order[] = [];
   currentOrderDetails: string[] = ['none'];
   loginStatus: boolean;
@@ -35,11 +31,6 @@ export class LoggedInComponent implements OnInit {
   //     'Content-Type': 'application/json',
   //     'Authorization': 'Bearer '
   //   })
-//   Array(3) [ {…}, {…}, {…} ]
-// 0: Object { id: 1, user_id: "1", status: "complete" }
-// 1: Object { id: 2, user_id: "1", status: "complete" }
-// 2: Object { id: 3, user_id: "1", status: "active" }​
-// length: 3
 
   apiServer: string = environment.API_SERVER_IP;
   apiPort: string = environment.API_PORT;
@@ -58,18 +49,11 @@ export class LoggedInComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.orderService.getOrders().subscribe(res => {
-      this.currentOrders = res;
-    });
-    console.log('complete', this.currentOrders.filter(el =>{
-      el.status = 'complete';
-    }))
-    this.noOfCompletedOrders = this.currentOrders.filter(el => {
-      
-      el.status = 'complete'
-    });
-    console.log('noOfcompletedorders', this.noOfCompletedOrders);
-    console.log('current orders', this.currentOrders);
+    this.existingOrdersService();
+    // this.noOfCompletedOrders = this.currentOrders.filter(item => {
+    //   item.status == 'complete';
+    // });
+    this.completedOrders(this.currentOrders);
     this.updateLoginStatus();
     this.tokenService.getToken();
   }
@@ -91,33 +75,35 @@ export class LoggedInComponent implements OnInit {
       this.tokenService.getToken().subscribe(res => {
               this.username = res.user;
             })
+    });
+  }
+
+  existingOrdersService () {
+    this.orderService.getOrders().subscribe(res => {
+      this.currentOrders = res;
+      this.completedOrders(res);
+    });
+  }
+  
+  completedOrders(currentOrders: Order[]) {
+    this.noOfCompletedOrders = currentOrders.filter(item => {
+      return item.status == "complete";
     })
-    console.log('logged-in component loginStatus', this.loginStatus);
   }
 
   onSubmit(): void {
     this.loading = true;
-    
-    // console.log('this current orders',this.orderService.getOrders());
     this.orderService.getOrders().subscribe(res => {
       this.currentOrders = res;
-      // console.log('this.currentOrders',this.currentOrders);
-    })
-    this.noOfCompletedOrders = this.currentOrders.filter(el => {
-      el.status = 'complete'
     });
-    console.log('noOfcompletedorders', this.noOfCompletedOrders);
-    // this.noOfCompletedOrders;
-    // this.returnedJWT = this.getLogin(this.username, this.password);
+    this.noOfCompletedOrders = Array.from(this.currentOrders.filter(el => {
+      return el.status == 'complete'
+    })); 
     this.updateLoginStatus();
     this.loading = false;
     // this.router.navigate(['loggedin', {relativeTo: this.route}])
     // this.router.navigate(['loggedin'])
   }
-
-  // allUsersOrders(): Observable<Order[]> {
-    
-  // }
 
   orderDetails(oid:number): void {
     console.log('orderDetails', oid);
