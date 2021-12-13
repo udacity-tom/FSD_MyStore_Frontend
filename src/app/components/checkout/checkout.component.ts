1ac84137d3eb63c284b19fd7ba88a46c36d3cd61
udacity-tom <udacity@iamtom.com>
2021-12-13 23:50:55 +0100

    Feat: first draft Registration/Checkout page

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
+import { OrdersService } from 'src/app/service/orders.service';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  title: string = 'Checkout';
+  text: string = 'Please input your address to checkout';
+  firstname: string = '';
+  lastname: string = '';
+  houseNumber: string = '';
+  streetAddress: string = '';
+  streetAddress2: string = '';
+  city: string = '';
+  postcode: string = '';
+  country: string = '';
 
-  constructor() { }
+
+  // returnedJWT: object = {};
+  // token: string = '';
+  // authFn: object = {};
+  loading: boolean = false;
+  // stream: Observable<string> = '';
+  loginStatus: boolean = false;
+
+
+  constructor(private cartService: CartService, private orderService: OrdersService) { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    console.log('Checkout Submit button pressed!');
+    //start checkout process, update DB, print summary confirmation.    
+  }
 }
