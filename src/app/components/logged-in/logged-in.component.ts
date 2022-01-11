0c2067112203ed1e7115f495084362e389a43256
udacity-tom <udacity@iamtom.com>
2022-01-11 18:35:32 +0100

    Fix: observable return varaible renamed/added typedefs/harmonised identically equal operators

--- src/app/components/logged-in/logged-in.component.ts
+++ src/app/components/logged-in/logged-in.component.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
 import { Order } from 'src/app/models/Order';
 import { Observable } from 'rxjs';
 import { OrdersService } from 'src/app/service/orders.service';
-import { Order_products } from 'src/app/models/Order_products';
+import { OrderProducts } from 'src/app/models/OrderProducts';
 // import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
@@ -16,18 +16,18 @@ import { Order_products } from 'src/app/models/Order_products';
   styleUrls: ['./logged-in.component.css']
 })
 export class LoggedInComponent implements OnInit {
-  username: string = '';
-  password: string = '';
+  @Input() currentOrderDetails: OrderProducts[] = [ {id: 0, productid: 0, quantity: 0, orderid: 0}];
+  username = '';
+  password = '';
   returnedJWT: object = {};
-  token: string = '';
+  token = '';
   authFn: object = {};
-  loading: boolean = false;
+  loading = false;
   currentOrders: Order[] = [];
   noOfCompletedOrders: Order[] = [];
-  @Input() currentOrderDetails: Order_products[] = [ {id: 0, product_id: 0, quantity: 0, order_id: 0}];
-  showOrderDetails: boolean = false;
+  showOrderDetails = false;
   loginStatus: boolean;
-  order: Order = {id: 0, user_id: 0, status: ''};
+  order: Order = {id: 0, userId: 0, status: ''};
 
 
   apiServer: string = environment.API_SERVER_IP;
@@ -35,13 +35,13 @@ export class LoggedInComponent implements OnInit {
   protocol: string = environment.PROTOCOL;
 
   constructor(
-    private http: HttpClient, 
-    private loginService: LoginService, 
-    private tokenService: TokenService, 
-    private router: Router, 
+    private http: HttpClient,
+    private loginService: LoginService,
+    private tokenService: TokenService,
+    private router: Router,
     private route: ActivatedRoute,
     private orderService: OrdersService
-    ) { 
+    ) {
     this.loginStatus = false;
 
   }
@@ -64,62 +64,62 @@ export class LoggedInComponent implements OnInit {
   updateLoginStatus(): void {
     this.loginService.loginStatus().subscribe(res => {
       this.loginStatus = res;
-      if(!this.loginStatus){
+      if (!this.loginStatus){
         console.log('logged-in component re-route page, this.loginStatus is ', this.loginStatus);
         // this.router.navigate(['/', {relativeTo: this.route}]);
         this.router.navigate(['/']);
         return;
       }
-      this.tokenService.getToken().subscribe(res => {
-              this.username = res.user;
-            })
+      this.tokenService.getToken().subscribe(token => {
+              this.username = token.user;
+            });
     });
   }
 
-  existingOrdersService () {
+  existingOrdersService(): void {
     this.orderService.getOrders().subscribe(res => {
       console.log('logged-in component, res from this.orderService.getOrders() observable', res, res.length);
-      if(res.length == 0 || res == undefined){
+      if (res.length === 0 || res === undefined){
 
-        this.orderService.createOrder().subscribe(res => {
-          console.log('orderService res in logged-in ', res);
-          const orderServiceResult = res;
+        this.orderService.createOrder().subscribe(order => {
+          console.log('orderService res in logged-in ', order);
+          const orderServiceResult = order;
           return orderServiceResult;
         });
         this.existingOrdersService ();
-      } 
+      }
       this.currentOrders = res;
       this.completedOrders(res);
-      console.log('this.noOfCompletedOrders',this.noOfCompletedOrders);
+      console.log('this.noOfCompletedOrders', this.noOfCompletedOrders);
     });
   }
-  
-  completedOrders(currentOrders: Order[]) {//just return the length, not all the objects
+
+  completedOrders(currentOrders: Order[]): void {// just return the length, not all the objects
     this.noOfCompletedOrders = currentOrders.filter(item => {
-      return item.status == "complete";
-    })
+      return item.status === 'complete';
+    });
   }
 
-  orderDetails(oid:number): void {
+  orderDetails(oid: number): void {
     this.orderService.orderDetails(oid).subscribe(res => {
       console.log('orderDetails method on logged-in component res from orderDetails', res);
       this.currentOrderDetails = res;
     });
     this.showOrderDetails = true;
-    if( this.currentOrderDetails.length ==0) {
+    if ( this.currentOrderDetails.length === 0) {
       alert('You haven\'t added anything to your order yet. Visit our products page' );
     }
     console.log('orderDetails', oid);
   }
 
 
-  logout():void {
+  logout(): void {
     console.log('logout() run from logged-in component');
     this.tokenService.deleteToken();
-    this.router.navigate(['/'])
+    this.router.navigate(['/']);
   }
 
-  // updateLoginStatus():void { 
+  // updateLoginStatus():void {
   //   this.loginAuth.loginStatus().subscribe(res => {
   //     this.loginStatus = res;
   //     this.tokenService.getToken().subscribe(res => {
