18f5970a9336afdf13e6942d9af6b9d94bc70944
udacity-tom <udacity@iamtom.com>
2022-01-13 18:20:45 +0100

    Refactor: column name changed/homogenised with FE/BE app

--- migrations/sqls/20210917223741-order-products-table-up.sql
+++ migrations/sqls/20210917223741-order-products-table-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE order_products(
 id SERIAL PRIMARY KEY, 
-product_id bigint REFERENCES products(id), 
+productId bigint REFERENCES products(id), 
 quantity integer,
-order_id bigint REFERENCES orders(id) ON DELETE CASCADE
+orderId bigint REFERENCES orders(id) ON DELETE CASCADE
 );
--- migrations/test/sqls/20210919203039-add-orders-up.sql
+++ migrations/test/sqls/20210919203039-add-orders-up.sql
@@ -1,9 +1,9 @@
-INSERT INTO orders(id, status, user_id) VALUES(1, 'complete', '1');
-INSERT INTO orders(id, status, user_id) VALUES(2, 'complete', '1');
-INSERT INTO orders(id, status, user_id) VALUES(3, 'active', '1');
-INSERT INTO orders(id, status, user_id) VALUES(4, 'complete','2');
-INSERT INTO orders(id, status, user_id) VALUES(5, 'active', '2');
-INSERT INTO orders(id, status, user_id) VALUES(6, 'active', '5');
-INSERT INTO orders(id, status, user_id) VALUES(7, 'complete', '5');
-INSERT INTO orders(id, status, user_id) VALUES(8, 'complete', '5');
+INSERT INTO orders(id, status, userId) VALUES(1, 'complete', '1');
+INSERT INTO orders(id, status, userId) VALUES(2, 'complete', '1');
+INSERT INTO orders(id, status, userId) VALUES(3, 'active', '1');
+INSERT INTO orders(id, status, userId) VALUES(4, 'complete','2');
+INSERT INTO orders(id, status, userId) VALUES(5, 'active', '2');
+INSERT INTO orders(id, status, userId) VALUES(6, 'active', '5');
+INSERT INTO orders(id, status, userId) VALUES(7, 'complete', '5');
+INSERT INTO orders(id, status, userId) VALUES(8, 'complete', '5');
 ALTER SEQUENCE orders_id_seq RESTART WITH 9;
