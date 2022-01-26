b46ca2da371d555153d4ec3cf2d97f44f1d199c9
udacity-tom <udacity@iamtom.com>
2022-01-26 10:31:57 +0100

    Feat: notification added/enhance cart layout

--- src/app/components/cart-content/cart-content.component.ts
+++ src/app/components/cart-content/cart-content.component.ts
@@ -3,15 +3,17 @@ import {
   Input,
   OnInit,
   EventEmitter,
-  Output
+  Output,
+  OnChanges,
+  SimpleChange,
+  SimpleChanges
 } from '@angular/core';
 import {
   Product
 } from 'src/app/models/Product';
-import {
-  OrdersService
-} from 'src/app/service/orders.service';
-
+import { 
+  ToastService 
+} from 'src/app/service/toast.service';
 
 interface CartProduct extends Product {
   quantity: number;
@@ -40,21 +42,31 @@ export class CartContentComponent implements OnInit {
   };
   // @Input() product: Product;
   @Input() cart: CartProduct[] = [];
-  @Output() removeItemFromCart: EventEmitter < CartProduct > = new EventEmitter();
+  @Output() removeItemFromCart: EventEmitter < CartProduct > = new EventEmitter;
   currentCartStatus = false;
   titleText1 = 'See Product Description ->';
 
-  constructor(private ordersService: OrdersService) {}
+  constructor(private toastService: ToastService) {}
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(): void {
+    this.cart = []
+  }
+
   removeItem(): void {
+    console.log('cart-content comp, this.product', this.product);
     this.removeItemFromCart.emit(this.product);
-    // location.reload();
+    
     setTimeout(() => {
-      location.reload();
+      // location.reload();
+      window.location.reload();
+      this.ngOnInit();
     }, 100);
+    setTimeout(() => {
+      this.toastService.show(`${this.product.name} was removed from the cart`, `The product ${this.product.name} was removed from the cart.`)
+    },250);
   }
 
 }
