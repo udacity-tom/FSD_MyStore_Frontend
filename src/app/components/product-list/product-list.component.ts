15a8f7237b9257bb9942a3161e230c5f25d1f030
udacity-tom <udacity@iamtom.com>
2022-01-26 11:51:54 +0100

    Fix: remove cartService, add loginService for statemanagement of child

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input, OnInit} from '@angular/core';
 import { ProductService } from 'src/app/service/products.service';
 import { Product } from 'src/app/models/Product';
-import { CartService } from 'src/app/service/cart.service';
+import { LoginService } from 'src/app/service/login.service';
 import { OrdersService } from 'src/app/service/orders.service';
-
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -11,20 +10,41 @@ import { OrdersService } from 'src/app/service/orders.service';
 })
 export class ProductListComponent implements OnInit {
   title = 'Product List component';
-  @Input() products: Product[] = [];
+  // @Input() 
+  products: Product[] = [];
+  @Input() activeOrder: number = 0;
   pid = 0;
   quantity = 0;
   response: object = {};
-  activeOrder = 0;
+  // activeOrder = 0;
+  loginStatus = false;
 
-  constructor(private productService: ProductService, private cartService: CartService, private orderService: OrdersService) { }
+  constructor(private productService: ProductService, private loginService: LoginService, private orderService: OrdersService ) { }
 
   ngOnInit(): void {
-    // raw data return for setting up View, move to backend later
-    this.productService.getProducts().subscribe(res => {
-      this.products = res;
-    });
+    this.getActiveOrder();
+    // this.orderService.activeOrder().subscribe(res => {
+    //   this.activeOrder = res.id;
+    //   console.log('product-list, this.activeOrder, res', this.activeOrder, res);
+    // })
+    if(this.activeOrder === 0){
+
+    }
+
+    setTimeout(() => {
+      this.loginService.loginStatus().subscribe(status => {
+        this.loginStatus = status;
+      })
+      this.productService.getProducts().subscribe(res => {
+        this.products = res;
+      });
+       }, 70);
+    
+    console.log('product-list comp this.loginStatus, this.activeOrder', this.loginStatus, this.activeOrder);
   }
+
+
+
 
   // addProductsToCart(item: {pid:number, quantity:number}) {
   //   console.log('product-list compont, item', item);
@@ -35,11 +55,11 @@ export class ProductListComponent implements OnInit {
   //   })
   // }
 
-  // getActiveOrder() {
-  //   this.orderService.currentActiveOrder().subscribe(res => {
-  //     this.activeOrder = res;
-  //     console.log('product-item-detail, this.activeOrder, res', this.activeOrder, res);
-  //   });
-  // }
+  getActiveOrder() {
+    this.orderService.activeOrder().subscribe(res => {
+      this.activeOrder = res.id;
+      console.log('product-list, this.activeOrder, res', this.activeOrder, res);
+    });
+  }
 
 }
