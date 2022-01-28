d2527dd95e2430c41373e165015fad95a5ce2184
udacity-tom <udacity@iamtom.com>
2022-01-28 19:17:00 +0100

    Refactor: cleanup move logout() action to loginService

--- src/app/components/logged-in/logged-in.component.ts
+++ src/app/components/logged-in/logged-in.component.ts
@@ -1,22 +1,19 @@
-import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/service/login.service';
 import { TokenService } from 'src/app/service/token.service';
-import { Router, ActivatedRoute } from '@angular/router';
-import { HttpClient, HttpHeaders, HttpInterceptor } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { Order } from 'src/app/models/Order';
-import { Observable } from 'rxjs';
 import { OrdersService } from 'src/app/service/orders.service';
 import { OrderProducts } from 'src/app/models/OrderProducts';
 import { ProductService } from 'src/app/service/products.service';
 import { Product } from 'src/app/models/Product';
-// import { NavbarComponent } from '../navbar/navbar.component';
-
 @Component({
   selector: 'app-logged-in',
   templateUrl: './logged-in.component.html',
   styleUrls: ['./logged-in.component.css']
 })
+
 export class LoggedInComponent implements OnInit {
   @Input() currentOrderDetails: OrderProducts[] = [ {id: 0, productid: 0, quantity: 0, orderid: 0}];
   username = '';
@@ -49,11 +46,9 @@ export class LoggedInComponent implements OnInit {
   protocol: string = environment.PROTOCOL;
 
   constructor(
-    private http: HttpClient,
     private loginService: LoginService,
     private tokenService: TokenService,
     private router: Router,
-    private route: ActivatedRoute,
     private orderService: OrdersService,
     private productsService: ProductService
     ) {
@@ -86,10 +81,8 @@ export class LoggedInComponent implements OnInit {
 
   existingOrders(): void {
     this.orderService.getOrders().subscribe(res => {
-      // console.log('logged-in component, res from this.orderService.getOrders() observable', res, res.length);
       if (res.length === 0 || res === undefined){
         this.orderService.createOrder().subscribe(order => {
-          console.log('orderService res in logged-in ', order);
           const orderServiceResult = order;
           return orderServiceResult;
         });
@@ -97,8 +90,6 @@ export class LoggedInComponent implements OnInit {
       }
       this.currentOrders = res;
       this.completedOrders(res);
-      console.log('this.noOfCompletedOrders', this.noOfCompletedOrders);
-      
     });
   }
 
@@ -111,26 +102,18 @@ export class LoggedInComponent implements OnInit {
   orderDetails(oid: number): void {
     this.products = [];
     this.orderService.orderDetails(oid).subscribe(res => {
-      console.log('orderDetails method on logged-in component res from orderDetails', res);
       this.currentOrderDetails = res;
       res.forEach(item => {
         this.productsService.getProduct(item.productid).subscribe(product => {
           this.products.push(product);
         })
       });
-
     });
     this.showOrderDetails = true;
-    if ( this.currentOrderDetails === []) {
-      // alert('You haven\'t added anything to your order yet. Visit our products page' );
-    }
-    console.log('orderDetails', oid);
-    console.log('orderDetails, this.products', this.products);
   }
 
   logout(): void {
-    console.log('logout() run from logged-in component');
-    this.tokenService.deleteToken();
+    this.loginService.logOut(this.firstname);
     this.router.navigate(['/']);
   }
 }
