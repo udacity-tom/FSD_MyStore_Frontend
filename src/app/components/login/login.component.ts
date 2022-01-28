57815d309d58140a5dfe2a6c1dfc8583770ce611
udacity-tom <udacity@iamtom.com>
2022-01-28 17:27:11 +0100

    Feat: link register button/register() method/login notification-success,fail

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/service/login.service';
 import { TokenService } from 'src/app/service/token.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { ToastService } from 'src/app/service/toast.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -20,7 +21,7 @@ export class LoginComponent implements OnInit {
     private loginService: LoginService,
     private tokenService: TokenService,
     private router: Router,
-    private route: ActivatedRoute) {
+    private toastService: ToastService) {
 
   }
 
@@ -33,7 +34,6 @@ export class LoginComponent implements OnInit {
       return;
     }
     this.router.navigate(['loggedin']);
-
   }
 
   onSubmit(): void {
@@ -45,6 +45,10 @@ export class LoginComponent implements OnInit {
 
   reRouteUser(): void {
     this.router.navigate(['loggedin']);
+  }
+
+  register(): void {
+    this.router.navigate(['register']);
   }
 
   updateLoginStatus(): void {
@@ -62,10 +66,12 @@ export class LoginComponent implements OnInit {
       this.tokenService.setToken(this.returnedJWT);
       this.updateLoginStatus();
       if(this.loginStatus) {
-        //notify user successful login
+        this.toastService.show(`Login Successful`, 
+        `Welcome back ${this.username}. You successfully logged in!`);
         this.reRouteUser();
       } else {
-        // notify user unsuccessful login
+        this.toastService.show(`Login Unsuccessful!`, 
+        `Sorry! Do you already have a user account? If not, register first!`);
         this.username = '';
         this.password = '';
         this.router.navigate(['/login']);
