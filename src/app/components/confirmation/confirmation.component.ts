c7ac36974576512421c41c72189950e37cd05546
udacity-tom <70481612+udacity-tom@users.noreply.github.com>
2022-02-21 13:42:38 +0100

    Refactor: remove unused service and logic

--- src/app/components/confirmation/confirmation.component.ts
+++ src/app/components/confirmation/confirmation.component.ts
@@ -6,9 +6,6 @@ import {
   User
 } from 'src/app/models/User';
 import {
-  OrdersService
-} from 'src/app/service/orders.service';
-import {
   UserService
 } from 'src/app/service/user.service';
 
@@ -32,23 +29,13 @@ export class ConfirmationComponent implements OnInit {
     country: ''
   };
   activeOrder = 0;
-  completedOrder: number = this.activeOrder - 1;
 
-  constructor(private orderService: OrdersService, private userService: UserService) {}
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.userId = this.userService.getUserId();
     this.userService.showUser(this.userId).subscribe(res => {
-      // console.log('confirmation comp res from show user', res);
       this.currentUser = res;
     });
-    this.orderService.getOrders().subscribe(orders => {
-      console.log('confirmation comp, orders,this.activeOrder, this.completedOrder', orders, this.activeOrder, this.completedOrder);
-    })
-
   }
-  // showPreviousOrder(): void
-  /*find current orders, take the last but one order
-  print it out on screen confirmation
-  */
 }
