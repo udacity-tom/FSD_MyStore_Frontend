d11a6ff757f4c393344177f4c1ee7d06c3cb1521
udacity-tom <70481612+udacity-tom@users.noreply.github.com>
2022-02-22 10:13:49 +0100

    Formatting/Fix: whitespace/return types, remove import

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { ProductService } from 'src/app/service/products.service';
 import { Product } from 'src/app/models/Product';
 import { LoginService } from 'src/app/service/login.service';
@@ -12,36 +12,42 @@ import { ToastService } from 'src/app/service/toast.service';
 export class ProductListComponent implements OnInit {
   title = 'Our List Of Prouducts';
   products: Product[] = [];
-  activeOrder: number = 0;
+  activeOrder = 0;
   loginStatus = false;
   pid = 0;
   quantity = 0;
   response: object = {};
 
-  constructor(private productService: ProductService, private loginService: LoginService, private orderService: OrdersService, private toastService: ToastService ) { }
+  constructor(
+    private productService: ProductService,
+    private loginService: LoginService,
+    private orderService: OrdersService,
+    private toastService: ToastService
+    )
+    { }
 
   ngOnInit(): void {
     this.getActiveOrder();
     setTimeout(() => {
       this.loginService.loginStatus().subscribe(status => {
         this.loginStatus = status;
-      })
+      });
       this.productService.getProducts().subscribe(res => {
         this.products = res;
       });
        }, 100);
   }
 
-  addProductToCart(item: {pid:number, quantity:number}) {
+  addProductToCart(item: {pid: number, quantity: number}): void {
     this.getActiveOrder();
     this.orderService.addProductToActiveOrder(item.pid, item.quantity, this.activeOrder).subscribe(res => {
       this.response = res;
-      const productIndex = item.pid-1;
+      const productIndex = item.pid - 1;
       this.toastService.show(`Add to Cart: ${this.products[productIndex].name}`, `We have put ${item.quantity} piece${(item.quantity > 1 ? 's' : '')} of the product '${this.products[productIndex].name}' into your Cart!` );
-    })
+    });
   }
 
-  getActiveOrder() {
+  getActiveOrder(): void {
     this.orderService.activeOrder().subscribe(res => {
       this.activeOrder = res.id;
     });
