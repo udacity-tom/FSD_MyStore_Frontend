8f9e669a6b94a557f2f7b9e47c5b3bb817e42b99
udacity-tom <70481612+udacity-tom@users.noreply.github.com>
2022-02-22 11:29:56 +0100

    Formatting: whitespace

--- src/app/service/token.service.ts
+++ src/app/service/token.service.ts
@@ -1,10 +1,5 @@
-import {
-  Injectable
-} from '@angular/core';
-import {
-  of ,
-  Observable
-} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { of, Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
