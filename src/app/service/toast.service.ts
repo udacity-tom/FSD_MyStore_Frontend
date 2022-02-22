892a160a8c4c769315585ef6d43dd6769c8b267f
udacity-tom <70481612+udacity-tom@users.noreply.github.com>
2022-02-22 10:18:32 +0100

    Fix/Formatting: add method return types, equal sign/whitespace, semicolon

--- src/app/service/toast.service.ts
+++ src/app/service/toast.service.ts
@@ -3,31 +3,31 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class ToastService {
-  toast: any
-  toasts:any[] = [];
-  
-  
+  toast: any;
+  toasts: any[] = [];
+
   constructor() { }
-  
-  show(header: string, body: string) {
+
+  show(header: string, body: string): void {
     const delay = 12000;
     const sentToast = this.toasts.push({header, body, delay});
     setTimeout( () => {
       this.remove(sentToast);
-    }, delay+15000);
+    }, delay + 15000);
     }
 
-  remove(toast: any) {
-    this.toasts = this.toasts.filter(t => t != toast);
+  remove(toast: any): void {
+    this.toasts = this.toasts.filter(t => t !== toast);
     setTimeout( () => {
-      this.delete(toast)
+      this.delete(toast);
     }
     , 25000);
   }
 
-  delete(toast: any) {
-    let index = this.toasts.indexOf(toast);
+  delete(toast: any): void {
+    const index = this.toasts.indexOf(toast);
     this.toasts.splice(index, 1);
   }
 
