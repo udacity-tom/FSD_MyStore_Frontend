d2b83760833076974b4a79225fcbd85644a475e0
udacity-tom <70481612+udacity-tom@users.noreply.github.com>
2022-03-24 23:54:22 +0100

    Refactor: move product function from order service to here->efficacious and speedy

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/Product';
 import { OrderProducts } from 'src/app/models/OrderProducts';
-import { Order } from 'src/app/models/Order';
+import { Observable, of } from 'rxjs';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/service/login.service';
 import { TokenService } from 'src/app/service/token.service';
 import { OrdersService } from 'src/app/service/orders.service';
+import { ProductService } from 'src/app/service/products.service';
 interface CartProduct extends Product {
   quantity: number;
   order_productsId: number;
@@ -17,38 +18,41 @@ interface CartProduct extends Product {
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+ 
+ orderIdNum: Observable<number>;
 
- cart: CartProduct[] = [];  // products in cart
- cartOrder: Order = {id: 0, userId: 0, status: ''}; // the current user cart to update to DB onChange
+ cart: CartProduct[] = new Array;
+ cart2: CartProduct[] = new Array;
  cartTotal = 0.00;
- currentOrder: Order = {id: 0, userId: 0, status: ''}; // The DB order where status ='active'
- allOrders: Order[] = []; // all user orders on DB
- activeOrderNum = 0;
- orderProducts: OrderProducts[] = [{id: 0, productid: 0, quantity: 0, orderid: 0}]; // order transactions
+
  product: Product = {id: 0, name: '', url: '', price: 0, snippet: '', description: '', accreditation: '', category: '' };
+ products: CartProduct[] = [];
+ currentOrderDetails: OrderProducts[] = [ {id: 0, productid: 0, quantity: 0, orderid: 0}];
  loginStatus = false;
  username = '';
- addedCartItem = 0;
  currentCartStatus = false;
- itemsInCart = 0;
+ titleText1 = 'See Product Description ->';
+ activeOrderNum = 0;
 
  constructor(
   private loginService: LoginService,
   private tokenService: TokenService,
   private router: Router,
   private ordersService: OrdersService,
-  ) { }
+  private productsService: ProductService
+  ) {
+    this.orderIdNum = this.ordersService.orderIdNum;
+    const numOfActiveOrder = this.activeOrder();
+   }
 
-  ngOnInit(): void {
+   ngOnInit(): void {
     this.updateLoginStatus(); // check if user is logged in, (if yes update cart with active order)
-    // get all orders on system BE for user
-    this.productsInActiveCart();
   }
-
-  onChanges(): void {
-    this.productsInActiveCart();
+  
+  ngOnChanges(): void {
+    // this.activeOrder();
   }
-
+  
   updateLoginStatus(): void {
     this.loginService.loginStatus().subscribe(res => {
       this.loginStatus = res;
@@ -61,35 +65,48 @@ export class CartComponent implements OnInit {
       });
     });
   }
-
-  productsInActiveCart(): void { //  pushes products in active order to cart.
-    this.ordersService.cartContent.subscribe(res => {
-      console.log('cart.component res from cartContent', res);  
-      this.cart = res;
-      res.forEach(item => {
-        console.log('cart component', item);
-        this.cartTotal += Number(item.price) * Number(item.quantity);
-      });
+  
+  activeOrder(): number {
+    let tempOrderNum: number = -1;
+    this.ordersService.activeOrder().subscribe( orderNum => {
+      this.activeOrderNum = orderNum.id;
+      tempOrderNum = orderNum.id;
+      this.orderDetails(tempOrderNum);
     })
-    this.cart.forEach(item => {
-      console.log('cart component ', item);
-    })
-    if (this.orderProducts.length !== 0) {
-      this.currentCartStatus = true;
-    }
+    return tempOrderNum;
   }
-
-  removeItemFromCart(product: CartProduct): boolean {
+  
+  
+  orderDetails(oid: number): void {
+    this.products = [];
+    this.cartTotal = 0;
+    this.ordersService.orderDetails(oid).subscribe(res => {
+      this.currentOrderDetails = res;
+      res.forEach(item => {
+        this.productsService.getProduct(item.productid).subscribe(product => {
+          product = Object.assign(product, {quantity: item.quantity, order_productsId: item.id, orderId: item.orderid});
+          this.products.push(product);
+          this.cartTotal += Number(product.price) * Number(product.quantity);
+        });
+      });
+      if (this.currentOrderDetails.length !== 0) {
+        this.currentCartStatus = true;
+      } else {
+        this.currentCartStatus =false;
+      }
+    });
+    this.ordersService.countCartItems(this.activeOrderNum);
+  }
+  
+  removeItemFromCart(product: CartProduct): void {
     this.ordersService.removeCartItem(product.quantity, product.orderId, product.id, product.order_productsId).subscribe(res => {
-      // const wasDeleted = res;
       if (Number(res.productid) === product.id) {
         alert(`'${product.name}' was removed from the cart!`);
       }
-      location.reload();
+      this.activeOrder();
     });
-    return false;
   }
-
+  
   checkout(): void{
     this.router.navigate(['/checkout']);
   }
