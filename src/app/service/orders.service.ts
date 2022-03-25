01da04641164bb6d13975cef2425b654fc048a17
udacity-tom <70481612+udacity-tom@users.noreply.github.com>
2022-03-25 11:10:44 +0100

    Feat: add countCartItems Beh Sub/small format changes

--- src/app/service/orders.service.ts
+++ src/app/service/orders.service.ts
@@ -6,6 +6,7 @@ import { Product } from '../models/Product';
 import { Order } from '../models/Order';
 import { OrderProducts } from '../models/OrderProducts';
 import { TokenService } from './token.service';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -18,29 +19,46 @@ export class OrdersService {
       Authorization: 'Bearer '
     })
   };
-  currentOrder: Order = {
-    id: 0,
-    userId: 0,
-    status: ''
-  }; // The DB order where status ='active'
-  allOrders: Order[] = []; // all user orders on DB
-  justOne: Order[] = [];
   orderProducts: OrderProducts[] = [{id: 0, productid: 0, quantity: 0, orderid: 0}]; // order transactions
+  product: Product = {id: 0, name: '', url: '', price: 0, snippet: '', description: '', accreditation: '', category: '' };
   productId = 0;
   orderId = 0;
   url = '';
+  // activeOrderNumber = 0;
+  cartCount = 0;
+  
+  orderIdNum: Subject<number> = new BehaviorSubject<number>(0);
   cartItems: Subject<number> = new BehaviorSubject<number>(0);
-
 
   constructor(
     private http: HttpClient,
     private tokenService: TokenService
-    ) {}
+    ) {
+      this.activeOrder().subscribe( orderNum => {
+        this.setOrderIdNum(orderNum.id);
+        this.countCartItems(orderNum.id);
+      });
+    }
 
   setCartItems(numberOfCartItems: number): void {
     this.cartItems.next(numberOfCartItems);
-    console.log('order service cart items', numberOfCartItems);
-    console.log('order service cart items', this.cartItems);
+  }
+
+  setOrderIdNum(currentOrderId: number): void {
+    this.orderIdNum.next(currentOrderId);
+  }
+
+  countCartItems(oid: number): void {
+    this.cartCount = 0;
+    this.orderDetails(oid).subscribe(product => {
+      product.forEach(item => {
+        this.cartCount += item.quantity;
+        this.setCartItems(this.cartCount);
+      });
+    });
+    if(this.cartCount === 0){
+      this.setCartItems(this.cartCount);
+    }
   }
 
   getOrders(): Observable < Order[] > { // gets the results of orders DB
@@ -53,35 +71,35 @@ export class OrdersService {
       }]); // if user not logged in return order zero
     }
     const request = this.http.get < Order[] > (`${this.baseURL}/users/` +
-      this.jwtToken.uid + '/orders', this.httpOptions);
+    this.jwtToken.uid + '/orders', this.httpOptions);
     return request;
   }
-
+  
   createOrder(userId ?: string, status ?: string): Observable < Order > {
     this.addAuthorisation();
     const request = this.http.post < Order > (`${this.baseURL}/users/` +
-      this.jwtToken.uid + '/orders/create', {
-        userId,
-        status
-      }, this.httpOptions);
+    this.jwtToken.uid + '/orders/create', {
+      userId,
+      status
+    }, this.httpOptions);
     return request;
   }
-
+  
   completeOrder(oid: number): Observable < Order > {
     this.addAuthorisation();
     const body = {}; // required for http put request (modification) RFC7231
     const request = this.http.put < Order > (`${this.baseURL}/users/` +
-      this.jwtToken.uid + '/orders/' + oid, body, this.httpOptions);
+    this.jwtToken.uid + '/orders/' + oid, body, this.httpOptions);
     return request;
   }
-
+  
   orderDetails(oid: number): Observable < OrderProducts[] > {
     this.addAuthorisation();
     const request = this.http.get < OrderProducts[] > (`${this.baseURL}/users/` +
-      this.jwtToken.uid + '/orders/' + oid, this.httpOptions);
-    return request;
-  }
-
+    this.jwtToken.uid + '/orders/' + oid, this.httpOptions);
+      return request;
+    }
+    
   addProductToActiveOrder(pid: number, quantity: number, oid: number): Observable < object > {
     this.productId = pid;
     this.activeOrder();
@@ -93,14 +111,11 @@ export class OrdersService {
     this.url = `${this.baseURL}/users/${this.jwtToken.uid}/orders/${oid}/add-product`;
     return this.http.post < Product > (this.url, body, this.httpOptions);
   }
-
+    
   removeCartItem(quantity: number, oid: number, productid: number, opid: number): Observable < {
-    id: number,
-    quantity: number,
-    orderId: number,
-    productid: number,
-    order_productId: number
-  } > {
+      id: number, quantity: number, orderId: number, productid: number, order_productId: number
+    } > {
+      this.activeOrder();
     this.addAuthorisation();
     const body = {
       id: productid,
@@ -113,10 +128,10 @@ export class OrdersService {
       productid: number,
       order_productId: number
     } > (`${this.baseURL}/users/` + this.jwtToken.uid + '/orders/' + oid +
-      '/delete-product/' + opid, body, this.httpOptions);
+    '/delete-product/' + opid, body, this.httpOptions);
     return request;
   }
-
+  
   activeOrder(): Observable < Order > {
     this.addAuthorisation();
     if (this.jwtToken.uid === 0) { // gets round error of calling API for orders when not logged in
@@ -133,12 +148,12 @@ export class OrdersService {
     }
     return request;
   }
-
+  
   addAuthorisation(): void {
     this.currentToken(); // invoke method to update token before submission to API
     this.httpOptions.headers = this.httpOptions.headers.set('Authorization', 'Bearer ' + this.jwtToken.token);
   }
-
+  
   currentToken(): void {
     this.tokenService.getToken().subscribe(res => {
       this.jwtToken = res;
