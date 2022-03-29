7f393d446443290047214a39275c77193e63328e
udacity-tom <70481612+udacity-tom@users.noreply.github.com>
2022-03-29 12:19:55 +0200

    Refactor/Style: lint compliance/code clarity

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { DashboardService } from 'src/app/service/dashboard.service';
 import { Product } from 'src/app/models/Product';
 import { ProductService } from 'src/app/service/products.service';
-import { Router } from '@angular/router';
 
 interface PopularProduct extends Product {
   sum: number;
@@ -15,9 +14,8 @@ interface PopularProduct extends Product {
 export class AboutComponent implements OnInit {
   products: PopularProduct[] = [];
   constructor(
-    private dashboardService: DashboardService, 
-    private productsService: ProductService,
-    private router: Router
+    private dashboardService: DashboardService,
+    private productsService: ProductService
     ) {
     }
 
@@ -27,16 +25,15 @@ export class AboutComponent implements OnInit {
 
   topProducts(): void {
     this.products = [];
-    this.dashboardService.topFiveProducts().subscribe(res => {
-      res.forEach(item => {
-        const productidValue = Object.values(item)[0];
-        const sumValue = Object.values(item)[1];
-        this.productsService.getProduct(productidValue).subscribe(res => {
-          this.products.push(res);
-          this.products[this.products.length-1].sum = sumValue;
-          this.products.sort( (a, b) => b.sum - a.sum); 
+    this.dashboardService.topFiveProducts().subscribe( topFive => {
+      topFive.forEach(item => {
+        const productidValue = Object.values(item)[0]; // productId
+        const sumValue = Object.values(item)[1]; // total sold-sumvalue from postgres
+        this.productsService.getProduct(productidValue).subscribe(product => {
+          this.products.push(Object.assign(product, {sum: sumValue}));
         });
       });
+      this.products.sort( (a, b) => b.sum - a.sum);
     });
   }
 }
