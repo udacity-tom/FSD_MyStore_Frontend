f5cbf78a616d2ef0c00afeff6108837226b14f52
udacity-tom <70481612+udacity-tom@users.noreply.github.com>
2022-03-30 15:37:58 +0200

    Refactor: user details retrieval->display details at checkout

--- src/app/components/confirmation/confirmation.component.ts
+++ src/app/components/confirmation/confirmation.component.ts
@@ -1,21 +1,13 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
-import {
-  User
-} from 'src/app/models/User';
-import {
-  UserService
-} from 'src/app/service/user.service';
-
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { UserService } from 'src/app/service/user.service';
+import { OrderProducts } from 'src/app/models/OrderProducts';
 @Component({
   selector: 'app-confirmation',
   templateUrl: './confirmation.component.html',
   styleUrls: ['./confirmation.component.css']
 })
 export class ConfirmationComponent implements OnInit {
-  userId = 0;
   currentUser: User = {
     id: 0,
     username: '',
@@ -29,12 +21,12 @@ export class ConfirmationComponent implements OnInit {
     country: ''
   };
   activeOrder = 0;
+  activeOrderDetails: OrderProducts[] = [];
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
-    this.userId = this.userService.getUserId();
-    this.userService.showUser(this.userId).subscribe(res => {
+    this.userService.showUser(this.userService.getUserId()).subscribe(res => {
       this.currentUser = res;
     });
   }
